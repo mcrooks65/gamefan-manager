52d193734a55299550b029898456b5dcdc78562f
Matthew Crooks <mfcrooks@gmail.com>
2018-07-03 12:25:08 -0700

    Add validations to Developer model

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,8 @@
 class Developer < ApplicationRecord
   has_many :games
   has_many :fans, through: :games
+
+  validates :name, presence: true, uniqueness: true
+  validates :employees, presence: true,  numericality: { greater_than: 0 } 
+  validates :location, presence: true
 end
