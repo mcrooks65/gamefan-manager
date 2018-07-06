6ee7978de0edb5994f8c8d80e5394a58b1cd28fe
Matthew Crooks <mfcrooks@gmail.com>
2018-07-06 15:50:45 -0700

    Add password_digest to Developer migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_041429) do
+ActiveRecord::Schema.define(version: 2018_07_06_224910) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_041429) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "fans", force: :cascade do |t|
