7dfb1b694369cc57ad28fc12989110130feee41c
Matthew Crooks <mfcrooks@gmail.com>
2018-07-06 17:25:44 -0700

    Add password validations to Dev model

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -6,6 +6,7 @@ class Developer < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :employees, presence: true,  numericality: { greater_than: 0 } 
   validates :location, presence: true
+  validates :password, presence: true, length: {within: 5..30}
 
   def self.most_fans
     self.select { |dev| dev.fans.count }
