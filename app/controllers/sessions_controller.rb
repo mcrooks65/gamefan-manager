cfb3eaf09380a9d6200baa587d848cb2e9c29e2d
Matthew Crooks <mfcrooks@gmail.com>
2018-07-11 10:34:32 -0700

    Add sessions controller and new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  def destroy
+    session.destroy
+    redirect_to '/'
+  end
+
   def new
+    @dev = Developer.new
+    @devs = Developer.all
+  end
+
+  def create
+    @dev = Developer.find_by(id: params[:developer][:id])
+    if @dev && @dev.authenticate(params[:developer][:password]) #Why won't this line work?
+      session[:dev_id] = @dev.id
+      redirect_to @dev
+    else
+      redirect_to '/login'
+    end
   end
 end
