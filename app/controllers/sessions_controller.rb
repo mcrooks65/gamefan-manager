3924086ab6f2f07f5207fa890b07f78bcc076011
Matthew Crooks <mfcrooks@gmail.com>
2018-07-11 13:35:44 -0700

    Work on Oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,15 +10,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-
-    if user.valid?
+    #OAUTH START
+    dev = Developer.from_omniauth(env["omniauth.auth"])
+    if dev.valid?
       session[:user_id] = user.id
       redirect_to request.env['omniauth.origin']
     end
-
+    #OAUTH END
     @dev = Developer.find_by(id: params[:developer][:id])
-    if @dev && @dev.authenticate(params[:developer][:password]) #Why won't this line work?
+    if @dev && @dev.authenticate(params[:developer][:password])
       session[:dev_id] = @dev.id
       redirect_to @dev
     else
