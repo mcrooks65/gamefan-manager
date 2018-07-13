8fe517feb497d170eb72c80783ab55482e54d0a7
Matthew Crooks <mfcrooks@gmail.com>
2018-07-13 14:15:15 -0700

    Add uid to developers db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_224910) do
+ActiveRecord::Schema.define(version: 2018_07_13_190623) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_07_06_224910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "uid"
   end
 
   create_table "fans", force: :cascade do |t|
