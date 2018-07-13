b50a0d5c0b8c282c2567269527843688acdc7657
Matthew Crooks <mfcrooks@gmail.com>
2018-07-13 10:14:59 -0700

    Update app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_dev
 
-  def current_user
-    session[:user_id].nil? ? nil : User.find(session[:user_id])
+  def current_dev
+    session[:dev_id].nil? ? nil : Developer.find(session[:dev_id])
   end
 end
